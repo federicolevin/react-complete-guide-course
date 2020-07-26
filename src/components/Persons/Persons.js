28147e18b36a5c30805a6cf269fa60dff1df0f00
Federico Levin <federico.levin@despegar.com>
2020-07-26 15:53:34 -0300

    Lesson 78: Component update lifecycle (for props changes)

--- src/components/Persons/Persons.js
+++ src/components/Persons/Persons.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, {Component} from "react";
 import Person from "./Person/Person";
 
-const persons = (props) => {
-  console.log('[Persons.js] rendering...')
+class Persons extends Component {
 
-  return props.persons.map((person, index) => {
-    return <Person key={person.id} name={person.name} age={person.age} click={() => props.clicked(index)} changed={(event) => props.changed(event, person.id)} />
-  });
+  shouldComponentUpdate(nextProps, nextState, nextContext) {
+    console.log('[Persons.js] shouldComponentUpdate');
+    return true;
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('[Persons.js] getSnapshotBeforeUpdate');
+    return {message: 'Snapshot!'};
+  }
+
+  componentDidUpdate(prevProps, prevState, snapshot) {
+    console.log('[Persons.js] componentDidUpdate');
+    console.log(snapshot);
+  }
+
+  render() {
+    console.log('[Persons.js] rendering...')
+
+    return this.props.persons.map((person, index) => {
+      return <Person key={person.id} name={person.name} age={person.age} click={() => this.props.clicked(index)} changed={(event) => this.props.changed(event, person.id)}/>
+    });
+  }
 }
 
-export default persons;
+export default Persons;
